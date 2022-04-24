df001ce25bebd4f83ffca145ae7d6e225d0ff4fe
kimsangjae <zzai_sang@naver.com>
2022-04-24 23:13:04 +0900

    init

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2022/03/15
+ */
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+    }
+
+}
